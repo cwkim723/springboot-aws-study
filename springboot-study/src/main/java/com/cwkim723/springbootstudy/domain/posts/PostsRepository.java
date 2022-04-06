0ed9841c4d49b3d9a6f544405e41e4fb2ce61d28
cwkim723 <jerry.pref@gmail.com>
2022-04-07 01:49:19 +0900

    feat: 게시글 목록

--- springboot-study/src/main/java/com/cwkim723/springbootstudy/domain/posts/PostsRepository.java
+++ springboot-study/src/main/java/com/cwkim723/springbootstudy/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.cwkim723.springbootstudy.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // entity 클래스와 기본 entity repository는 함께 위치해야 함
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- springboot-study/src/main/java/com/cwkim723/springbootstudy/service/PostsService.java
+++ springboot-study/src/main/java/com/cwkim723/springbootstudy/service/PostsService.java
@@ -2,12 +2,16 @@ package com.cwkim723.springbootstudy.service;
 
 import com.cwkim723.springbootstudy.domain.posts.Posts;
 import com.cwkim723.springbootstudy.domain.posts.PostsRepository;
+import com.cwkim723.springbootstudy.web.dto.PostsListResponseDto;
 import com.cwkim723.springbootstudy.web.dto.PostsResponseDto;
 import com.cwkim723.springbootstudy.web.dto.PostsSaveRequestDto;
 import com.cwkim723.springbootstudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,6 +35,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
