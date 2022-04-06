81a8bdc3b87af0b4d08036b91db3bd5249f3de14
cwkim723 <jerry.pref@gmail.com>
2022-04-07 00:23:35 +0900

    feat: mustache 화면 구현

---
+++ springboot-study/src/main/java/com/cwkim723/springbootstudy/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.cwkim723.springbootstudy.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
